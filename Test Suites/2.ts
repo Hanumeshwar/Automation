f2206016cdd6b1609433874cb4b45bf4f2998356
hanum <hanum@LAPTOP-3EIU86EU>
2023-12-05 15:46:26 +0530

    22

---
+++ Test Suites/2.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>14c4a3ad-f1d3-4999-8d80-c7c3fa5c249d</testSuiteGuid>
+</TestSuiteEntity>
